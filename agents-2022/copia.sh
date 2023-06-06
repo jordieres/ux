7ae3b2ded5a61370ec3b37aa4846414cf16ec4b1
J.Ordieres <j.ordieres@upm.es>
2023-06-06 23:50:59 +0200

    Clean files & forlders v 4.3.1

--- agents-2022/copia.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-scp *.py jb@apiict00.etsii.upm.es:/home/jb/soft/dynreact/
-scp *.py jb@apiict03.etsii.upm.es:/home/jb/soft/dynreact/
-scp *.py jb@pcudp03.etsii.upm.es:/home/jb/agents_jb03/
-scp *.py jb@pcudp03.etsii.upm.es:/home/jb/agents_jb00/
-
--- agents-2022/va_ord.sh
+++
@@ -1,5 +0,0 @@
-nohup python3 va.py -an 8 -sd 0.25 -lag log@apiict00.etsii.upm.es -bag browser@apiict00.etsii.upm.es -u va08@apiict00.etsii.upm.es -p DynReact &
-nohup python3 va.py -an 9 -sd 0.25 -lag log@apiict00.etsii.upm.es -bag browser@apiict00.etsii.upm.es -u va09@apiict00.etsii.upm.es -p DynReact &
-nohup python3 va.py -an 10 -sd 0.25 -lag log@apiict00.etsii.upm.es -bag browser@apiict00.etsii.upm.es -u va10@apiict00.etsii.upm.es -p DynReact &
-nohup python3 va.py -an 11 -sd 0.25 -lag log@apiict00.etsii.upm.es -bag browser@apiict00.etsii.upm.es -u va11@apiict00.etsii.upm.es -p DynReact &
-nohup python3 va.py -an 12 -sd 0.25 -lag log@apiict00.etsii.upm.es -bag browser@apiict00.etsii.upm.es -u va12@apiict00.etsii.upm.es -p DynReact &
